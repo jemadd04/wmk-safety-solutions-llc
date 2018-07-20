1d7a11140c5226663946d0611d135982192e6714
James Maddox <james.maddox07@gmail.com>
2018-07-19 23:49:11 -0400

    Completed footer area

--- js/message.js
+++ js/message.js
@@ -1,15 +1,5 @@
-$('#submitMessage').hide();
-$('#submitMessage').fadeIn();
-
-// $('#previewButton').click(function() {
-//   const title = $('#blogTitleInput').val();
-//   const content = $('#blogContentInput').val();
-//
-//   $('#blogTitlePreview').text(title);
-//   $('#blogContentPreview').html(content);
-//
-//   $('#flashMessage')
-//     .slideDown(1000)
-//     .delay(2000)
-//     .slideUp();
-//   });
+$(document).ready(function() {
+  $("#formSubmit").click(function() {
+      alert("Thanks! We will contact you shortly.")
+  })
+});
